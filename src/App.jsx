bd3c4c5580cdd07ca606ae60c5e4bea80f952802
AmrKhaledNaser <amrkna6@gmail.com>
2026-02-03 03:01:41 +0200

    fix router basename for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AppLayout from './components/AppLayout';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AppLayout />} />
+    </Routes>
   );
 }
